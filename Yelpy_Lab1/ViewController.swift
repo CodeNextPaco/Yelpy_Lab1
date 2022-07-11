06fea01ae9c16c4987503e70055004252c187fd0
coach paco <paco@csedge.org>
2022-07-10 21:12:44 -0700

    Initial Commit

---
+++ Yelpy_Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yelpy_Lab1
+//
+//  Created by admin on 7/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
